044259d58ba792ad50e6a3f11f8ecbf9e4326585
Brett Boettcher <hgdxg@BrettsRTX>
2025-11-05 22:53:47 -0700

    removed key recording setup in keyboard service

--- src/app/services/keyboard.service.ts
+++ src/app/services/keyboard.service.ts
@@ -8,9 +8,7 @@ export class KeyboardService {
 	private _pressedKeys = new BehaviorSubject<Set<string>>(new Set());
 	pressedKeys$ = this._pressedKeys.asObservable(); // reactive observable
 
-	private recording = false;
 	private pressedKeys = new Set<string>();
-	private recordedKeys = [new Set<string>()]
 
 	pressKey(key: string) {
 		if (this.pressedKeys.has(key)) return;
@@ -21,17 +19,5 @@ export class KeyboardService {
 	releaseKey(key: string) {
 		this.pressedKeys.delete(key);
 		this._pressedKeys.next(this.pressedKeys);
-	}
-
-	startRecording() {
-		this.recording = true;
-	}
-
-	stopRecording() {
-		this.recording = false;
-	}
-
-	getRecordedKeys() {
-		return this.recordedKeys;
 	}
 }
